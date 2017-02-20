bc5b89ffee38438bb9d8dc6c97c1e9fe5f495166
Deepanshu Garg <deepanshugarg257@gmail.com>
2017-02-20 19:04:09 +0530

    implemented alpha beta but buggy

--- game.py
+++ game.py
@@ -7,43 +7,80 @@ class Player1:
 
     def __init__(self):
         self.termVal = 100
-        self.limit = 3
+        self.limit = 2
         self.count = 0
 
-    def heuristic(self,old_move):
-        return (50,old_move)
+    def heuristic(self, old_move):
+        return (50, old_move)
 
-    def dfs(self,board,old_move,flag,depth):
-        if(board.find_terminal_state()[0] == flag):
-            return (self.termVal,old_move)
-        if(board.find_terminal_state()[0] == 'x' or board.find_terminal_state()[0] == 'o'):
-            return (-1*self.termVal,old_move)
+    def alphaBeta(self, board, old_move, flag, depth, alpha, beta):
+        # Assuming 'x' to be the maximising player
+        
+        if(board.find_terminal_state()[0] == 'x'):
+            return (self.termVal, old_move)
+        
+        if(board.find_terminal_state()[0] == 'o'):
+            return (-1*self.termVal, old_move)
+        
         if(board.find_terminal_state()[0] == 'NONE' or depth > self.limit):
             return self.heuristic(old_move)
+        
         cells = board.find_valid_move_cells(old_move)
-        nodeVal = -5*self.termVal
-        toMark = [-1,-1]
-        for chosen in cells :
-            # print(chosen)
-            new = 'x'
-            if(flag == 'x'):
-                new = 'o'
+
+        if (flag == 'x'):
+            
+            nodeVal = -5*self.termVal, (-1,-1)
+            new = 'o'
             tmp = copy.deepcopy(board.block_status)
-            board.update(old_move,chosen,flag)
-            tmp1 = self.dfs(board,chosen,new,depth+1)
-            if(nodeVal<tmp1[0]):
-                nodeVal = tmp1[0]
-                toMark[0] = chosen[0]
-                toMark[1] = chosen[1]
+            
+            for chosen in cells :
+                board.update(old_move, chosen, flag)
+                tmp1 = self.alphaBeta(board, chosen, new, depth+1, alpha, beta)
+                if(nodeVal[0] < tmp1[0]):
+                    nodeVal = tmp1
+                    alpha = max(alpha, nodeVal[0])
+                    if beta <= alpha :
+                        break
+                board.board_status[chosen[0]][chosen[1]] = '-'
+                board.block_status = copy.deepcopy(tmp)
+                del(tmp1)
+                
             board.board_status[chosen[0]][chosen[1]] = '-'
             board.block_status = copy.deepcopy(tmp)
             del(tmp)
-        return (nodeVal,toMark)
+                
+            return nodeVal
 
-    def move(self,board,old_move,flag):
+
+        if (flag == 'o'):
+            
+            nodeVal = 5*self.termVal, (-1,-1)
+            new = 'x'
+            tmp = copy.deepcopy(board.block_status)
+            
+            for chosen in cells :
+                board.update(old_move, chosen, flag)
+                tmp1 = self.alphaBeta(board, chosen, new, depth+1, alpha, beta)
+                if(nodeVal[0] > tmp1[0]):
+                    nodeVal = tmp1
+                    beta = min(beta, nodeVal[0])
+                    if beta <= alpha :
+                        break
+                board.board_status[chosen[0]][chosen[1]] = '-'
+                board.block_status = copy.deepcopy(tmp)
+                del(tmp1)
+                
+            board.board_status[chosen[0]][chosen[1]] = '-'
+            board.block_status = copy.deepcopy(tmp)
+            del(tmp)
+                
+            return nodeVal
+
+
+    def move(self, board, old_move, flag):
         print("hey")
-        self.count+=1
-        print("entering the move for ",self.count)
-        toret = self.dfs(board,old_move,flag,1)[1]
-        print(toret)
-        return toret[0],toret[1]
+        self.count += 1
+        print("entering the move for ", self.count)
+        toret = self.alphaBeta(board, old_move, flag, 1, -1000, 1000)[1]
+        print("toret",toret)
+        return toret[0], toret[1]
